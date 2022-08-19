f013ffd045fca2f87dd496dd168dce842274afbc
Jean-Philippe Ouvrard <jp.ouvrard@lesoctetslibres.com>
2022-08-19 10:22:16 +0200

    fix(Processing and PIA): :bug: 1855

--- src/app/entry/entry-content/validate-pia/validate-pia.component.ts
+++ src/app/entry/entry-content/validate-pia/validate-pia.component.ts
@@ -151,7 +151,7 @@ export class ValidatePIAComponent implements OnInit {
     const simpleValidationBtn = document.getElementById('pia-simple-validation');
     const signValidationBtn = document.getElementById('pia-sign-validation');
 
-    [].forEach.call(radioButtons, function(currentRadioBtn) {
+    [].forEach.call(radioButtons, function (currentRadioBtn) {
       if (!currentRadioBtn.checked) {
         allBtnChecked = false;
       }
@@ -159,6 +159,9 @@ export class ValidatePIAComponent implements OnInit {
     if (allBtnChecked) {
       simpleValidationBtn.removeAttribute('disabled');
       signValidationBtn.removeAttribute('disabled');
+    } else {
+      simpleValidationBtn.setAttribute('disabled', 'true');
+      signValidationBtn.setAttribute('disabled', 'true');
     }
   }
 }
